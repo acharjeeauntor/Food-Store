37923c6cafea7ea73c0af039200d342e87321990
acharjeeauntor <acharjeeauntor@gmail.com>
2020-08-02 23:28:26 +0600

    Add fonts and navigator to the different screen.

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:foodstore/category_item.dart';
+import 'dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
-      children: <Widget>[],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Food-Store"),
+        ),
+        body: GridView(
+          padding: const EdgeInsets.all(20),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+          children: DUMMY_CATEGORIES
+              .map((catData) => CategoryItem(
+                    catData.id,
+                    catData.title,
+                    catData.color,
+                  ))
+              .toList(),
+        ),
+      ),
     );
   }
 }
